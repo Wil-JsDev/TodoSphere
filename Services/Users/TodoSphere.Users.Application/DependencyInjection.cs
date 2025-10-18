bdd69500a664e9843056a163f0e394b75f81a929
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-10-18 15:56:31 -0400

    feat(application): register UserService and RolesService in DI container

--- Services/Users/TodoSphere.Users.Application/DependencyInjection.cs
+++ Services/Users/TodoSphere.Users.Application/DependencyInjection.cs
@@ -1,5 +1,16 @@
-﻿namespace TodoSphere.Users.Application;
+﻿using Microsoft.Extensions.DependencyInjection;
+using TodoSphere.Users.Application.Interfaces.Services;
+using TodoSphere.Users.Application.Services;
 
-public class Class1
+namespace TodoSphere.Users.Application;
+
+public static class DependencyInjection
 {
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IRolesService, RolesService>();
+        services.AddScoped<IUserService, UserService>();
+
+        return services;
+    }
 }
