22ec2674d5b71362907f43c35132eb4af56ab6a6
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-10-18 15:56:59 -0400

    feat(infrastructure): register MongoDB context and repositories in DI

--- Services/Users/TodoSphere.Users.Infrastructure/DependencyInjection.cs
+++ Services/Users/TodoSphere.Users.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,38 @@
-﻿namespace TodoSphere.Users.Infrastructure;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using TodoSphere.Users.Application.Interfaces.Repositories;
+using TodoSphere.Users.Infrastructure.Context;
+using TodoSphere.Users.Infrastructure.Repositories;
 
-public class DependecyInjection
+namespace TodoSphere.Users.Infrastructure;
+
+public static class DependencyInjection
 {
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        #region DBContext
+
+        services.AddDbContext<TodoSphereUserContext>(option =>
+        {
+            var connectionString = configuration.GetConnectionString("ConnectionMongoDB");
+
+            var client = new MongoClient(connectionString);
+
+            option.UseMongoDB(client, "TodoSphereUsers");
+        });
+
+        #endregion
+
+        #region Repositories
+
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IRolesRepository, RolesRepository>();
+
+        #endregion
+
+        return services;
+    }
 }
